cec335865037a784c55b515186e06ddaef93bbd7
pavelman223 <rul3zzzzzz@gmail.com>
2025-03-18 02:09:06 +0600

    Commit

--- src/ResultsBoard.java
+++ src/ResultsBoard.java
@@ -1,41 +1,46 @@
 import java.util.*;
 
 public class ResultsBoard {
-    private TreeMap<Double, String> board = new TreeMap<>(Collections.reverseOrder());
+    private TreeMap<Float, List<String>> board = new TreeMap<>(Collections.reverseOrder());
 
-    public void addStudent(String name, Double score) {
-        // Предполагаем, что имена студентов уникальны
-        board.put(score, name);
+    public void addStudent(String name, Float score) {
+        // Получаем список студентов с данным баллом
+        board.putIfAbsent(score, new ArrayList<>());
+        board.get(score).add(name);
     }
 
     public List<String> top3() {
         List<String> topStudents = new ArrayList<>();
         int count = 0;
-        for(Map.Entry<Double, String> entry : board.entrySet()) {
-            if(count < 3) {
-                topStudents.add(entry.getValue());
-                count++;
-            } else {
+
+        for(Map.Entry<Float, List<String>> entry : board.entrySet()) {
+            for (String student : entry.getValue()) {
+                if (count < 3) {
+                    topStudents.add(student);
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            if (count >= 3) {
                 break;
             }
         }
+
         return topStudents;
     }
+
     public static void main(String[] args) {
         ResultsBoard board = new ResultsBoard();
-        board.addStudent("Иван", 4.5);
-        board.addStudent("Мария", 4.8);
-        board.addStudent("Алексей", 4.2);
-        board.addStudent("Светлана", 4.9);
-        board.addStudent("Николай", 4.7);
+        board.addStudent("Иван", 4.56f);
+        board.addStudent("Мария", 4.86f);
+        board.addStudent("Алексей", 4.86f); // Дубликат по баллу
+        board.addStudent("Светлана", 4.933f);
+        board.addStudent("Николай", 4.724f);
 
         List<String> topStudents = board.top3();
         for(String student : topStudents) {
             System.out.println(student);
         }
     }
-
-
-
 }
-
